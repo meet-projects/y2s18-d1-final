d2714324b4fce7e7bed825fdf40a1b5e7730f09f
aboud20-meet <aboud20@meet.mit.edu>
2018-08-13 16:26:15 +0300

    f

--- app.py
+++ app.py
@@ -18,21 +18,25 @@ def money():
     money=session.query(
        Donate).filter_by(
        needs=money).all()
+
 @app.route('/food')
 def food():
     return render_template('food.html')
     food=session.query(
        Donate).filter_by(
        needs=food).all()
+
 @app.route('/about_us')
 def aboutus():
     return render_template('about_us.html')
+
 @app.route('/essentials')
 def essentials():
     return render_template('essentials.html') 
     essentials =session.query(
        Donate).filter_by(
        needs=essentials).all()  
+
 @app.route('/contact_us')
 def contact():
     return render_template('contact_us.html')
@@ -53,6 +57,12 @@ def add():
        link=request.form['link']
 
 
-       save_to_database(name, animal) 
+        add_donate(name, story, email, needer_type, needs, phone_num, address, link)
+        return render_template('response.html',
+               n=name,
+               s=story,
+               e=email, nt=needer_type, ne=needs, p=phone_num, a=address, l=link)
+ 
+
 if __name__ == "__main__":
     app.run(debug=True)
