bcfcfd5b6ffc429f850a8689fb4f8bb5ecda3dc1
aboud20-meet <aboud20@meet.mit.edu>
2018-08-13 12:05:40 +0300

    dfghjk

--- app.py
+++ app.py
@@ -15,13 +15,9 @@ def home():
 @app.route('/money')
 def money():
     return render_template('money.html')
-    
-
-# Running the Flask app
-@app.route('/essentials')
-def essentials():
-	return render_template("essentials.html")
-
+    money=session.query(
+       Donate).filter_by(
+       needs=money).all()
 @app.route('/food')
 def food():
     return render_template('food.html')
@@ -36,6 +32,7 @@ def contact():
     return render_template('contact_us.html')
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+
     return render_template('add.html')
 if __name__ == "__main__":
     app.run(debug=True)
